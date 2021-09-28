af7ed4ca290d478efd3d6af6e11fb0e18fcd436d
Wim <w.j.stienstra@home.nl>
2021-09-28 15:57:20 +0200

    Fixed slider again, plane a bit closer

--- RayTracer/src/RayTracer18/Main.java
+++ RayTracer/src/RayTracer18/Main.java
@@ -3,12 +3,16 @@ package RayTracer18;
 
 import RayTracer18.Light.PointLight;
 import RayTracer18.Primitives.Material;
+import RayTracer18.Primitives.Plane;
 import RayTracer18.Primitives.Triangle;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -18,26 +22,49 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Button button;
+    Slider slider;
+    Label label;
 
     Scene3D scene = new Scene3D();
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Canvas canvas = new Canvas(600,200);
+    public void start(Stage primaryStage) throws Exception {
+        Canvas canvas = new Canvas(600, 200);
 
         primaryStage.setTitle("Ray tracer");
+
+
+        label = new Label();
+        label.setText(String.format("Field of View (FoV): %.1f ", Math.abs(.3 * 100 - 100)));
+
+        slider = new Slider();
+        slider.setMin(0);
+        slider.setMax(1);
+        slider.setValue(.3);
+        slider.setBlockIncrement(1);
+        slider.setMajorTickUnit(.1);
+        slider.setMinorTickCount(0);
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                scene.camera.setFov(newValue.doubleValue());
+                label.setText(String.format("Field of View (FoV): %.1f ", Math.abs((double) newValue * 100 - 100)));
+            }
+        });
+
         button = new Button();
         button.setText("Trace da rays");
-        button.setOnAction(e->{
-            Triangle t = new Triangle(new Vector3(-3, 0, 4), new Vector3(0,6,4), new Vector3(3, 0,4));
+        button.setOnAction(e -> {
+            Triangle t = new Triangle(new Vector3(-3, 0, 4), new Vector3(0, 6, 4), new Vector3(3, 0, 4));
             Triangle blocker = new Triangle(
-                    new Vector3(-0.3,1.5,1),
+                    new Vector3(-0.3, 1.5, 1),
                     new Vector3(0.3, 1.5, 1),
-                    new Vector3(0,2,1)
+                    new Vector3(0, 2, 1)
             );
             scene.add(blocker);
 
+            Plane p = new Plane(1);
+            scene.add(p);
             Material blue = new Material(Color.BLUE);
             t.applyMaterial(blue);
 
@@ -54,23 +81,18 @@ public class Main extends Application {
         GridPane gridPane = new GridPane();
         Button b1 = new Button();
 
-
-
-
-
-
-
         b1.setText("Yeet");
 
         Button b2 = new Button();
         b2.setText("hi2");
-        gridPane.addRow(1,b1);
+        gridPane.addRow(1, b1);
         gridPane.addRow(1, b2);
         gridPane.addRow(1, button);
+        gridPane.addRow(2, label);
+        gridPane.addRow(3, slider);
 
         GridPane mainc = new GridPane();
         mainc.getChildren().add(canvas);
-
 
 
         HBox statusbar = new HBox();

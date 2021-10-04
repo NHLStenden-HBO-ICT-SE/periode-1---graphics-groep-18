e774094d83398856bdc710b6207e1d4812c6576f
Wim <w.j.stienstra@home.nl>
2021-10-04 17:56:40 +0200

    Re-added light customizer

--- RayTracer/src/RayTracer18/Light/Light.java
+++ RayTracer/src/RayTracer18/Light/Light.java
@@ -23,4 +23,15 @@ public abstract class Light {
                 ", position=" + position +
                 '}';
     }
+    public void setPosition(Vector3 position) {
+        this.position = position;
+    }
+
+    public void setIntensity(double intensity) {
+        this.intensity = intensity;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
--- RayTracer/src/RayTracer18/Main.java
+++ RayTracer/src/RayTracer18/Main.java
@@ -10,25 +10,21 @@ import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class Main extends Application {
 
-    Button button;
-    Label label;
-    Slider slider;
+    BorderPane borderPane;
+    GridPane gridPane;
+    ColorPicker colorPicker;
 
     Scene3D scene = new Scene3D();
     Canvas canvas = new Canvas(600, 300);
@@ -46,14 +42,15 @@ public class Main extends Application {
     TreeItem<String> rootObjects = new TreeItem<String>("Objects");
     TreeItem<String> rootLights = new TreeItem<String>("Lights");
 
-    public void createHierarchy(){
+    public void createHierarchy() {
         ArrayList<String> objectList = new ArrayList<>();
         ArrayList<String> lightList = new ArrayList<>();
+
         objectList.add(scene.getObjects().toString());
         lightList.add(scene.getLights().toString());
         rootObjects.setExpanded(true);
         rootLights.setExpanded(true);
-        for (int i = 0; i < objectList.size(); i++){
+        for (int i = 0; i < objectList.size(); i++) {
             TreeItem<String> item = new TreeItem<String>(objectList.get(i));
             rootObjects.getChildren().add(item);
         }
@@ -63,46 +60,97 @@ public class Main extends Application {
         }
     }
 
+
+    private void customizeLights() {
+        var lights = scene.getLights();
+        var label = new Label();
+        label.setText("Lights");
+        gridPane.add(label, 4, 0);
+
+        for (int i = 0; i < lights.size(); i++) {
+            //Intensity slider
+            var slider = new Slider();
+            slider.setValue(lights.get(0).intensity);
+            slider.setMin(0f);
+            slider.setMax(10f);
+            slider.setValue(2f);
+            slider.setBlockIncrement(1);
+            slider.setMajorTickUnit(2);
+            slider.setMinorTickCount(1);
+            slider.setShowTickLabels(true);
+            slider.setSnapToTicks(true);
+            gridPane.add(slider, 5, i + 1);
+
+            //Colorpicker
+            var colorPicker = new ColorPicker();
+            colorPicker.setValue(lights.get(i).color);
+            gridPane.add(colorPicker, 3, i + 1);
+
+
+            //TODO: do something with position
+//            lights.get(i).setPosition(new Vector3(0,0,0));
+
+            int currentLight = i;
+            //Sets intensity of current light
+            slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+                lights.get(currentLight).setIntensity((Double) newValue);
+            });
+            //Sets color of current light
+            colorPicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+                lights.get(currentLight).setColor(newValue);
+            });
+
+        }
+
+        //Creates button and applies light changes
+        var button = new Button();
+        button.setText("Apply");
+        button.setOnAction(e -> {
+            System.out.println(lights);
+            Renderer.renderScene(scene, canvas);
+            createHierarchy();
+        });
+        gridPane.add(button, 4, 5);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        borderPane = new BorderPane();
+        gridPane = new GridPane();
 
         addMouseScrolling(canvas);
 
         primaryStage.setTitle("Ray tracer");
 
 
-        label = new Label();
+        Label label = new Label();
         label.setText(String.format("Field of View (FoV): %.1f ", Math.abs(.3 * 100 - 100)));
 
-        slider = new Slider();
+        Slider slider = new Slider();
         slider.setMin(0);
         slider.setMax(1);
         slider.setValue(.3);
         slider.setBlockIncrement(1);
         slider.setMajorTickUnit(.1);
         slider.setMinorTickCount(0);
-        slider.valueProperty().addListener(new ChangeListener<Number>() {
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                scene.camera.setFov(newValue.doubleValue());
-                label.setText(String.format("Field of View (FoV): %.1f ", Math.abs((double) newValue * 100 - 100)));
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            scene.camera.setFov(newValue.doubleValue());
+            label.setText(String.format("Field of View (FoV): %.1f ", Math.abs((double) newValue * 100 - 100)));
 
 
-                Renderer.renderScene(scene, canvas);
+            Renderer.renderScene(scene, canvas);
 
-            }
         });
 
-        button = new Button();
+        Button button = new Button();
         button.setText("Trace da rays");
         button.setOnAction(e -> {
             initRender(scene, canvas);
 
             createHierarchy();
+
+            customizeLights();
         });
-
-
-        BorderPane borderPane = new BorderPane();
-        GridPane gridPane = new GridPane();
 
         TreeView<String> tree = new TreeView<>(rootHierarchy);
         rootHierarchy.getChildren().add(rootObjects);
@@ -131,15 +179,14 @@ public class Main extends Application {
     }
 
 
-    public static void initRender(Scene3D scene, Canvas canvas){
+    public static void initRender(Scene3D scene, Canvas canvas) {
         Material blue = new Material(Color.BLUE);
         Material green = new Material(Color.GREEN);
         Material floorm = new Material(Color.ORANGE);
 
 
-
         Triangle t = new Triangle(
-                new Vector3(0,0,5),
+                new Vector3(0, 0, 5),
                 new Vector3(-3, 0, 5),
                 new Vector3(0, 6, 5),
                 new Vector3(3, 0, 5));
@@ -149,13 +196,14 @@ public class Main extends Application {
 
 
         //TODO: make this working correclty
-            Plane p = new Plane(new Vector3(0,-0.5,0), new Vector3(0,1,0));
-            scene.add(p);
-            p.applyMaterial(green);
-        Plane p2 = new Plane(new Vector3(0,10,0), new Vector3(0,1,0));
+
+        Plane p = new Plane(new Vector3(0, -0.5, 0), new Vector3(0, 1, 0));
+        scene.add(p);
+        p.applyMaterial(green);
+        Plane p2 = new Plane(new Vector3(0, 10, 0), new Vector3(0, 1, 0));
         scene.add(p2);
         p2.applyMaterial(green);
-        Plane p3 = new Plane(new Vector3(0,0,10), new Vector3(0,0,-1));
+        Plane p3 = new Plane(new Vector3(0, 0, 10), new Vector3(0, 0, -1));
         scene.add(p3);
         p3.applyMaterial(green);
 

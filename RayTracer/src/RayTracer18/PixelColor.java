cdc593cd2575f5de9cd77d31cb5e0b2df4440279
Eelco <eelco.jansma1@student.nhlstenden.com>
2021-10-07 11:50:04 +0200

    Updated PixelColor class

--- RayTracer/src/RayTracer18/PixelColor.java
+++ RayTracer/src/RayTracer18/PixelColor.java
@@ -13,30 +13,39 @@ public class PixelColor {
                 this.alpha = alpha;
         }
 
+        public double getRed() {return red * 255; }
+
+        public double getGreen() {return green * 255; }
+
+        public double getBlue() {
+                return blue * 255;
+        }
+
         public double getAlpha() {
                 return alpha;
         }
 
-        public double getRed() {
-                return red;
-        }
+        public static PixelColor mixColor(PixelColor c0, PixelColor c1){
 
-        public double getGreen() {
-                return green;
-        }
-
-        public double getBlue() {
-                return blue;
-        }
-
-        public static PixelColor mixColor(PixelColor c0, PixelColor c1, double weight0, double weight1){
+                double totalAlpha = c0.getAlpha() + c1.getAlpha();
+                double weight0 = c0.getAlpha() / totalAlpha;
+                double weight1 = c1.getAlpha() / totalAlpha;
 
                 double r = weight0 * c0.getRed() + weight1 * c1.getRed();
                 double g = weight0 * c0.getGreen() + weight1 * c1.getGreen();
                 double b = weight0 * c0.getBlue() + weight1 * c1.getBlue();
-                double a = Math.max(c0.getAlpha(), c1.getAlpha());
+                double a = totalAlpha;
 
-                return new PixelColor((int) r, (int) g, (int) b, (int) a);
+                return new PixelColor(r / 255, g / 255, b / 255, a);
+        }
 
+        @Override
+        public String toString() {
+                return "PixelColor{" +
+                        "red=" + red +
+                        ", green=" + green +
+                        ", blue=" + blue +
+                        ", alpha=" + alpha +
+                        '}';
         }
 }
